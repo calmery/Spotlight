2673c159db79cad8fd1f83f1d8f7672d7b15f955
Marei Kikukawa <contact@marei.jp>
2019-07-16 02:44:59 +0900

    ログの表示を変更，file から exists 関数をエクスポートする

--- src/core/modules/utility/debug.js
+++ src/core/modules/utility/debug.js
@@ -5,9 +5,9 @@ module.exports = (type, app, message) => {
   const time = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
   if (colors.hasOwnProperty(type)) {
-    console.log(colors[type](time, app, message));
+    console.log(colors[type](time, app, "-", message));
     return;
   }
 
-  console.log(time, app, message);
+  console.log(time, app, "-", message);
 };
