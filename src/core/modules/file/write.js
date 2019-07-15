bd94518f6a71d885ded3b7edfe0d0adebc3c48a4
Marei Kikukawa <contact@marei.jp>
2019-07-15 21:36:08 +0900

    存在しないディレクトリのファイルを指定したときに書き込みに失敗する問題を修正する

--- src/core/modules/file/write.js
+++ src/core/modules/file/write.js
@@ -1,9 +1,20 @@
 const fs = require("fs");
 const { isAbsolutePath } = require("../utility");
+const makeDirectory = require("./makeDirectory");
 
 module.exports = (path, content) => {
   if (!isAbsolutePath(path)) {
     return false;
+  }
+
+  if (path.split(/\/|\\/).length > 1) {
+    const _directoryPath = path.split(/\/|\\/);
+    _directoryPath.pop();
+    const directoryPath = _directoryPath.join("/");
+
+    if (!makeDirectory(directoryPath)) {
+      return false;
+    }
   }
 
   try {
