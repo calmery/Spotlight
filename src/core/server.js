9e0ee08c827b86eff0a68120026f63bfdbc6f839
Marei Kikukawa <contact@marei.jp>
2019-07-17 04:27:03 +0900

    Server クラスのフィールド名の変更と不要なメソッドの削除をする

--- src/core/server.js
+++ src/core/server.js
@@ -2,37 +2,37 @@ const express = require("express");
 
 class Server {
   constructor(port) {
-    this.app = express();
-    this.server = this.app.listen(port);
-    this.port = this.server.address().port;
+    this._express = express();
+    this._server = this._express.listen(port);
+    this._port = this._server.address().port;
   }
 
   use(...args) {
-    return this.app.use(...args);
+    return this._express.use(...args);
   }
 
   // app.get(route, function)
   get(...args) {
-    return this.app.get(...args);
+    return this._express.get(...args);
   }
 
   // app.post( route, function )
   post(...args) {
-    return this.app.post(...args);
+    return this._express.post(...args);
   }
 
   // app.put( route, function )
   put(...args) {
-    return this.app.put(...args);
+    return this._express.put(...args);
   }
 
   // app.delete( route, function )
   delete(...args) {
-    return this.app.delete(...args);
+    return this._express.delete(...args);
   }
 
   getPort() {
-    return this.port;
+    return this._port;
   }
 
   getUrl() {
@@ -41,10 +41,6 @@ class Server {
 
   getHost() {
     return `127.0.0.1:${this.getPort()}`;
-  }
-
-  getServer() {
-    return this.server;
   }
 }
 
