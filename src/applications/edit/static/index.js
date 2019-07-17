d3a5840ec990e67a6df1a2cf3cb0d7beccf00dcb
Marei Kikukawa <contact@calmery.me>
2019-07-18 00:23:15 +0900

    edit アプリケーションを追加する

---
+++ src/applications/edit/static/index.js
@@ -0,0 +1,117 @@
+function getJson(url, parameters) {
+  const u = new URL(`${window.location.origin}${url}`);
+  const p = new URLSearchParams();
+
+  if (parameters !== undefined) {
+    Object.entries(parameters).map(([key, value]) => p.append(key, value));
+  }
+
+  u.search = p.toString();
+  return fetch(u.toString());
+}
+
+function postJson(url, json) {
+  return fetch(url, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(json)
+  });
+}
+
+let data = null;
+
+function load(name) {
+  if (data !== null && !confirm("保存されていないデータは失われます")) {
+    return;
+  }
+
+  const menus = document.querySelectorAll('.file');
+  
+  for(let i=0; i<menus.length; i++) {
+    console.log(menus)
+    menus[i].className = "_menu file";
+  }
+
+  document.getElementById("output").innerHTML = "";
+  data = null;
+
+  getJson("/load", { name })
+    .catch(function() {
+      alert("データの読み込みに失敗しました");
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(tweets) {
+      data = tweets;
+      document.getElementById(`f-${name}`).className = "_menu file _active";
+      render(tweets);
+    });
+}
+
+function renderFiles(files) {
+  const element = document.getElementById("files");
+
+  element.innerHTML = "";
+
+  Object.entries(files).forEach(function(data) {
+    const fileName = data[0] + '.json';
+    const query = decodeURIComponent(data[1]);
+
+    element.innerHTML += `
+      <nav class="_navigator">
+        <div class="_menu file" id="f-${data[0]}" onclick="load('${data[0]}')">
+          <div class="_text">
+            ${query}
+            <div class="_description">${fileName}</div>
+          </div>
+        </div>
+      </nav>
+    `
+  })
+}
+
+function updateFiles() {
+  getJson("/all")
+    .catch(function () {
+      alert("ファイル一覧の取得に失敗しました")
+    })
+    .then(response => response.json())
+    .then(renderFiles)
+}
+
+updateFiles();
+
+function save() {
+  if (data === null) {
+    alert("検索データが存在しません");
+    return;
+  }
+
+  postJson("/save", data).then(function(response) {
+    if (response.status !== 200) {
+      alert("保存に失敗しました");
+      return;
+    }
+
+    alert("保存しました");
+  });
+}
+
+function toggle(id) {
+  const element = document.getElementById(`t-${id}`);
+  const index = parseInt(element.getAttribute("data-index"), 10);
+
+  if (element.getAttribute("data-is-dangerous") === "true") {
+    element.className = "_content _tweet _safe";
+    element.setAttribute("data-is-dangerous", "false");
+    data.statuses[index].is_dangerous = false;
+  } else {
+    element.className = "_content _tweet";
+    element.setAttribute("data-is-dangerous", "true");
+    data.statuses[index].is_dangerous = true;
+  }
+}
---
+++ src/applications/edit/static/render.js
@@ -0,0 +1,192 @@
+// Render Functions
+
+// ツイートを取得した後にこの関数が実行される
+function render(tweets) {
+  const output = document.getElementById("output");
+  const { statuses } = tweets;
+
+  output.innerHTML = "";
+  output.className = "";
+
+  statuses.forEach(function(status, index) {
+    const { user } = status;
+
+    // Reference: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/template_strings
+    output.innerHTML += `
+      <div
+        class="_content _tweet${status.is_dangerous ? "" : " _safe"}"
+        id="t-${status.id}"
+        data-index="${index}"
+        data-is-dangerous="${status.is_dangerous}"
+        onclick="toggle(${status.id})"
+      >
+        <div class="_profile">
+          <div class="_icon">
+            <img src="${user.profile_image_url}" />
+          </div>
+          <div class="_name">${user.name}</div>
+          <div class="_screen_name">@${user.screen_name}</div>
+          <div class="_id">${user.id}</div>
+        </div>
+
+        <div class="_text">
+          ${status.text}
+          ${renderMedia(status.extended_entities)}
+
+          <div class="_status">
+            <div class="_reaction">
+              <span class="_retweet">
+                <span class="_icon_font">}</span>
+                ${status.retweet_count}
+              </span>
+              <span class="_favorite">
+                <span class="_icon_font">—</span>
+                ${status.favorite_count}
+              </span>
+            </div>
+            <div class="_id">${status.id}</div>
+            <div class="_time">
+              ${new Date(status.created_at).toUTCString()}
+            </div>
+          </div>
+        </div>
+
+        ${renderEntities(status.entities)}
+      </div>
+    `;
+  });
+}
+
+// Reference: https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/extended-entities-object.html
+function renderMedia(extendedEntities) {
+  if (extendedEntities === undefined) {
+    return "";
+  }
+
+  return `
+    <div>
+      <div class="_media">
+        ${extendedEntities.media
+          .map(function(media) {
+            switch (media.type) {
+              case "photo":
+                return `
+                <img src="${media.media_url}" />
+              `;
+
+              case "animated_gif":
+              case "video":
+                return `
+                <video
+                  src="${(function() {
+                    const variant = media.video_info.variants.find(function(
+                      variant
+                    ) {
+                      return variant.content_type === "video/mp4";
+                    });
+
+                    if (variant) {
+                      return variant.url;
+                    }
+
+                    return media.video_info.variants[0].url;
+                  })()}"
+                  controls
+                  muted
+                  preload="metadata"
+                ></video>
+              `;
+
+              default:
+                return "";
+            }
+          })
+          .join("")}
+      </div>
+    </div>
+  `;
+}
+
+function renderEntities(entities) {
+  if (
+    entities.user_mentions.length === 0 &&
+    entities.hashtags.length === 0 &&
+    entities.urls.length === 0
+  ) {
+    return "";
+  }
+
+  return `
+    <div class="_column _tag_group">
+      ${renderUserMentions(entities.user_mentions)}
+      ${renderHashTags(entities.hashtags)}
+      ${renderUrls(entities.urls)}
+    </div>
+  `;
+}
+
+function renderUserMentions(userMentions) {
+  if (userMentions.length === 0) {
+    return "";
+  }
+
+  return `
+    <div>
+      <div class="_column">
+        ${userMentions
+          .map(function(userMention) {
+            return `
+            <span class="_tag _bg_primary">
+            <span class="_icon_font">ë</span> @${userMention.screen_name}
+            </span>
+          `;
+          })
+          .join("")}
+      </div>
+    </div>
+  `;
+}
+
+function renderHashTags(hashTags) {
+  if (hashTags.length === 0) {
+    return "";
+  }
+
+  return `
+    <div>
+      <div class="_column">
+        ${hashTags
+          .map(function(hashTag) {
+            return `
+            <span class="_tag _bg_primary">
+              <span class="_icon_font">a</span> ${hashTag.text}
+            </span>
+          `;
+          })
+          .join("")}
+      </div>
+    </div>
+  `;
+}
+
+function renderUrls(urls) {
+  if (urls.length === 0) {
+    return "";
+  }
+
+  return `
+    <div>
+      <div class="_column">
+        ${urls
+          .map(function(url) {
+            return `
+            <span class="_tag _bg_primary">
+              <span class="_icon_font">Y</span> ${url.url}
+            </span>
+          `;
+          })
+          .join("")}
+      </div>
+    </div>
+  `;
+}
