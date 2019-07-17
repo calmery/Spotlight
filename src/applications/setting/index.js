94466f2ebe4b97e1c85b78d4561b3754b4ecb9ce
Marei Kikukawa <contact@marei.jp>
2019-07-18 04:16:22 +0900

    データの削除の際に検索データも削除するように変更する

--- src/applications/setting/index.js
+++ src/applications/setting/index.js
@@ -1,6 +1,18 @@
 function main(application) {
   application.post("/remove", (_, response) => {
     application.removeSharedAppData("authentication.json");
+
+    // 検索データを削除する
+    let searchJson = application.loadSharedDocuments("search.json");
+    if (searchJson !== null) {
+      searchJson = JSON.parse(searchJson);
+      const fileNames = Object.keys(searchJson);
+      for (let i = 0; i < fileNames.length; i++) {
+        application.removeSharedDocuments(fileNames[i] + ".json");
+      }
+      application.removeSharedDocuments("search.json");
+    }
+
     response.status(200).end();
     process.exit(1);
   });
