702fbbc02717f1bed4403967bcbd22bf92f0dca4
Marei Kikukawa <contact@calmery.me>
2019-07-18 04:38:49 +0900

    ビルド用のスクリプトを追加する

---
+++ scripts/build.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path")
+const electronBuilder = require("electron-builder")
+
+// Build
+
+// NOTE: https://www.electron.build/configuration/win
+const buildForWindows = async () => {
+  const { build, Platform } = electronBuilder;
+
+  await build({
+    targets: Platform.WINDOWS.createTarget("nsis", electronBuilder.Arch.ia32),
+    config: {
+      productName: "Spotlight",
+      // NOTE: GitHub Releases に登録しないようにする
+      publish: null,
+      directories: {
+        app: path.resolve(__dirname, ".."),
+        output: path.resolve(__dirname, "../build/windows")
+      },
+      icon: path.resolve(__dirname, "../src/static/icon.icns")
+    }
+  });
+};
+
+// NOTE: https://www.electron.build/configuration/mac
+const buildForOSX = async () => {
+  const { build, Platform } = electronBuilder;
+
+  await build({
+    targets: Platform.MAC.createTarget(),
+    config: {
+      productName: "Spotlight",
+      // NOTE: GitHub Releases に登録しないようにする
+      publish: null,
+      directories: {
+        app: path.resolve(__dirname, ".."),
+        output: path.resolve(__dirname, "../build/osx")
+      },
+      // NOTE: http://www.img2icnsapp.com/
+      icon: path.resolve(__dirname, "../src/static/icon.icns")
+    }
+  });
+};
+
+// Main
+
+(async () => {
+  try {
+    await buildForWindows();
+    await buildForOSX();
+
+    console.log("Build Successful");
+  } catch (error) {
+    console.log(error.message || error.code || error);
+  }
+})();
