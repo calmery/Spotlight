1c542c0c8288edefc21a9de6fcb9c1bc7f885b18
Marei Kikukawa <contact@marei.jp>
2019-07-17 15:48:18 +0900

    setting アプリケーションを追加する

---
+++ src/applications/setting/static/main.js
@@ -0,0 +1,16 @@
+function removeAccount() {
+  if(confirm("認証情報，検索データを削除します")) {
+    postJson("/remove");
+  }
+}
+
+function postJson(url, json) {
+  return fetch(url, {
+    method: "POST",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(json)
+  });
+}
