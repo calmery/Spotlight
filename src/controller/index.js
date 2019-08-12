d1b249ced7256b6a98038e5d48f19f619f745b8a
Marei Kikukawa <contact@calmery.me>
2019-08-12 17:54:53 +0900

    ウインドウを生成するときに相対パスを引数として渡せるように変更する

--- src/controller/index.js
+++ src/controller/index.js
@@ -25,7 +25,9 @@ function createWindow(url, options) {
     url = getUrl();
   }
 
-  return window.create(url, options);
+  // 指定された URL が相対パスの場合，`getUrl()` 関数で得られる URL と連結する
+  // createWindow("controller.html") の場合は `http://127.0.0.1:生成されたポート番号/controller.html` となる
+  return window.create(new URL(url, getUrl()).href, options);
 }
 
 function close() {
